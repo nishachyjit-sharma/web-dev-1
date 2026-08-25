adf04f0b77578837c79c35c55ea681687a6267f7
navi <nish10@student.op.ac.nz>
2026-08-26 11:38:06 +1200

    loops lab

---
+++ html/loops.js
@@ -0,0 +1,30 @@
+// //task1 
+// let rugby1 = 1;
+// let rugby2 = 2;
+// while (rugby1 < 10){
+// let random = Math.floor(Math.random() * 5) + 1;
+// rugby1 += random;
+// rugby2 += random;
+// console.log("yoyo the score of team 1 now is " + rugby1);
+// console.log(" ");
+// console.log("yoyo the score of team 2 now is " + rugby1);
+// }
+// //task2
+// let input1 = Number(prompt("please enter your first number/alphabet"));
+// let input2 = Number(prompt("please enter your second number/alphabet"));
+// //task3
+// let number = Number(prompt("enter the number borski"));
+// for (i = 1; i < 13; i ++){
+//     let table = number * i;
+//     console.log(`${number} x ${i} = ${table}`);
+// }
+//task4
+let flip = 0;   
+while (flip < 1000){
+    let tails = 0;
+    let heads = 0;
+    let random = Math.floor(Math.random()*2);
+    
+}
+console.log(`the number of times tails has come is ${tails}`);
+console.log(`the number of times heads has come is ${heads}`);
